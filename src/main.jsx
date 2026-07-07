1dc4e9a7eb987fc3ec68ef5c97d9f28a2e42defc
iFamished <me@ifamished.com>
2026-07-07 16:12:56 +0000

    Violet

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/violet.css"
+import "ifamished-ui/styles/themes/solid/violet.css"
 
 // Site-specific background styles
 import "./styles/background.css"
