8bad781a32a2b70630cece42e7c24d92603f101a
iFamished <me@ifamished.com>
2026-07-07 16:14:36 +0000

    Ember

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom"
 
 // Shared UI package styles
 import "ifamished-ui/styles/index.css"
-import "ifamished-ui/styles/themes/solid/violet.css"
+import "ifamished-ui/styles/themes/fire/ember.css"
 
 // Site-specific background styles
 import "./styles/background.css"
